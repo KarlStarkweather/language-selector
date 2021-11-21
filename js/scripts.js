7c088ddd0e73a289d133b129baef8beb3808f424
KarlStarkweather <stark13@usa.net>
2021-11-21 00:36:56 -0800

    fixed logic and adjusted order of branches

--- js/scripts.js
+++ js/scripts.js
@@ -1,83 +1,65 @@
-$(document).ready(function() {
-  var userName;
+$(document).ready(function () {
   var mobileVal;
-  var objectifyVal;
   var coffeeVal;
   var musicVal;
   var reptilesVal;
   var peopleVal;
-  var langVal;
 
- 
-  $("#begin").click(function() {
-    $("#begin").hide();
-    $("#user-name").show();
-  });
-  $("#user-name-input").change(function() {
-    userName = $("#user-name").val();
-    $("#user-name").hide();
-    $("#mobile").show();
-  });
-  // first question
-  $("#mobile-select").change(function() {
-    mobileVal = $("#mobile-select").val(); 
-    $("#mobile").hide();
-    
-  });    
-  if (mobileVal === "Yes") {
-    $("#objectify").show();
-    $("#objectify-select").change(function() {
-    objectifyVal = $("#objectify-select").val();
-    $("#objectify").hide();
-    
-  });
-  $("#coffee").show();
-  $("#coffee-select").change(function() {
-    coffeeVal = $("#coffee-select").val();
-    $("#coffee").hide();
-  });
-  $("#music").show();
-  $("#music-select").change(function() {
-    musicVal = $("#music-select").val();
-    $("#music").hide();
-  });
-  $("#reptiles").show();
-    $("#reptiles-select").change(function() {
-    reptilesVal = $("#reptiles-select").val();
-    $("#reptiles").hide();
-    $("#people").show();
-  });
-  $("#people-select").change(function() {
-    peopleVal = $("#people-select").val();
-    $("#people").hide();
+  function showLanguage(lang) {
+     $("p").text("You should study " + lang + "!");
+  }
 
-    
-    if (mobileVal === "Yes") {
-      if (musicVal === "Yes") {
-        langVal = "C#";
-      } else {
-        if (coffeeVal === "coffee") {
-          langVal = "Javascript";
+  $("#begin").click(function () {
+    $("#begin").fadeOut();
+    $("#mobile").fadeIn();
+  });
+  
+  // first branch
+  $("#mobile-select").change(function () {
+    mobileVal = $("#mobile-select").val();
+    $("#mobile").fadeOut();
+    if (mobileVal === "Web") {
+      $("#reptiles").fadeIn(); //reptile question
+      $("#reptiles-select").change(function () {
+        reptilesVal = $("#reptiles-select").val();
+        $("#reptiles").fadeOut();
+        if (reptilesVal === "Yes") {
+          showLanguage("Python") ;
+        } else { // not reptiles, people?
+          $("#people").fadeIn();
+          $("#people-select").change(function () {
+            peopleVal = $("#people-select").val();
+            $("#people").fadeOut();
+            if (peopleVal === "Yes") {
+              showLanguage("React") ;
+            } else {
+              showLanguage("CSS") ;
+            }
+          });
+        };
+      });
+    } else { // Mobile apps
+      $("#coffee").fadeIn();
+      $("#coffee-select").change(function () {
+        coffeeVal = $("#coffee-select").val();
+        $("#coffee").fadeOut();
+        debugger
+        if (coffeeVal === "Coffee") {
+          showLanguage("JavaScript") 
         } else {
-          langVal = "English";
+          $("#music").fadeIn();
+          $("#music-select").change(function () {
+            musicVal = $("#music-select").val();
+            $("#music").fadeOut();
+            if (musicVal === "Yes") {
+              showLanguage("C#") ;
+            } else {
+              showLanguage("HTML") ;
+            }
+          });
         }
-      }
-    } else { //Not mobile app
-      if (reptilesVal === "Yes") {
-        langVal = "Python";
-      } else {
-        if (peopleVal === "Yes") {
-          langVal = "React";
-        } else {
-          langVal = "German";
-        }
-      }
+      });
     }
-    $("#result").show();
-    // alert(langVal);
-    $("p").text("You should study " + langVal + "!");
-      // event.preventDefault();
-      // alert(langVal);
   });
+  $("#result").show();
 });
-
